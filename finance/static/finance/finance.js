d56c7595d1ae84ecb5922a91cf98ac7d2742c0fc
Marco Paladini <paladini.marco@gmail.com>
2022-08-06 23:26:24 +0100

    single page navigation

--- finance/static/finance/finance.js
+++ finance/static/finance/finance.js
@@ -4,12 +4,16 @@ function get_accounts() {
                 row_div.attr('class', 'row');
                 var accounts_div = $('<div>').appendTo(row_div);
                 accounts_div.attr('class', 'one-half column');
-                accounts_div.attr('style', 'margin-top: 25%');
+                accounts_div.attr('style', 'margin-top: 5%; margin-left: 5%;');
+                var header = $('<h1>').appendTo(accounts_div);
+                header.text('Accounts');
                 $.each(resp['accounts'], function(key, value) {
                     console.log(key + " : " + JSON.stringify(value, null, 2));
-                    var tmp_span = $('<span>').appendTo(accounts_div);
-                    var button = $('<button>').appendTo(tmp_span);
-                    button.click(function () {expand_account(value['id']);});
+                    var button = $('<button>').appendTo(accounts_div);
+                    $('<br>').appendTo(accounts_div);
+                    button.click(function () {
+                        $('#container').empty();
+                        expand_account(value['id']);});
                     button.text(value['name']);
                 });
             });
@@ -19,5 +23,17 @@ function get_accounts() {
             console.log('opening account: ' + id);
             $.getJSON("/accounts/"+id, function(resp) {
               console.log(JSON.stringify(resp));
+              var row_div = $('<div>').appendTo('#container');
+              row_div.attr('class', 'row');
+              var details_div = $('<div>').appendTo(row_div);
+              details_div.attr('class', 'one-half column');
+              details_div.attr('style', 'margin-top: 5%; margin-left: 5%;');
+              var button = $('<button>').appendTo(details_div);
+              button.text('back');
+              button.click(function() {
+                    $('#container').empty();
+                    get_accounts();});
+              var header = $('<h1>').appendTo(details_div);
+              header.text('Details for '+resp['name']);
             });
         }
--- finance/views.py
+++ finance/views.py
@@ -13,4 +13,5 @@ def accounts(request):
 
 
 def account_view(request, account_id):
-    return JsonResponse({'test': account_id})
+    acc = get_object_or_404(Account, id=account_id)
+    return JsonResponse({'name': acc.name, 'id': acc.id})
